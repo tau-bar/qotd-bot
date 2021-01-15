44255422255ca28d8c877c3be882fbcbbd1114ba
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2021-01-15 21:21:01 +0800

    Changed from polling to webhooks

--- bot.py
+++ bot.py
@@ -1,21 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
+import os
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -34,6 +18,9 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+
+PORT = int(os.environ.get('PORT', '8443'))
+TOKEN = "1452285290:AAETh1BiY2nykuLVp0eh5i9QEIpZL3VvDXg"
 
 
 # Define a few command handlers. These usually take the two arguments update and
@@ -81,7 +68,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("1452285290:AAETh1BiY2nykuLVp0eh5i9QEIpZL3VvDXg", use_context=True)
+    updater = Updater(TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -100,7 +87,11 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN)
+
+    updater.bot.set_webhook("qotd-bot" + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
