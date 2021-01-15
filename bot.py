f442508d9b660a9a1987c78fa65adab5791ca6f0
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2021-01-15 21:30:17 +0800

    Fixed bug.

--- bot.py
+++ bot.py
@@ -80,9 +80,6 @@ def main():
     dp.add_handler(CommandHandler('addquote', add))
     dp.add_handler(CommandHandler('about', about))
 
-    # on noncommand i.e message - echo the message on Telegram
-    # dp.add_handler(MessageHandler(Filters.text, echo))
-
     # log all errors
     dp.add_error_handler(error)
 
@@ -91,7 +88,7 @@ def main():
                           port=PORT,
                           url_path=TOKEN)
 
-    updater.bot.set_webhook("qotd-bot" + TOKEN)
+    updater.bot.set_webhook("iqotd-bot" + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
